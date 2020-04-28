04d1fc90df1da23c8d9427ebb90d878e396239f8
MarkHooks <hooksma@ssfs.org>
2020-04-28 10:21:01 -0400

    finised cominatorics

---
+++ src/Combinatorics.java
@@ -0,0 +1,31 @@
+public class Combinatorics {
+
+    /** Precondition: n is between 1 and 12, inclusive.
+     * Returns the factorial of n, as described in part (a). */
+    public static int factorial(int n)
+    { /* to be implemented in part (a) */
+        int factorial =0;
+        if(n == 1){
+            factorial =1;
+        }else{
+            factorial = n* factorial(n-1);
+
+        }
+        return factorial;
+    }
+
+
+    /** Precondition: n and r are between 1 and 12, inclusive. * Determines the number of ways r items can be selected
+     * from n choices and prints the result, as described in part
+     (b). */
+    public static void numCombinations(int n, int r)
+    { /* to be implemented in part (b) */
+        int combo;
+        if(r>n){
+            combo =0;
+        }else {
+            combo = factorial(n)/(factorial(r) * factorial(n-r));
+        }
+        System.out.println("There are "+combo+" ways of choosing " + r +" items from "+ n+" choices.");
+    }
+}
